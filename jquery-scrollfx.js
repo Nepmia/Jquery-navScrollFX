b6b3f5ab31c54732c8a6407afb93237dc77b48b2
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-04-01 11:57:00 +0200

    final commit for script, made it universal just gonna edit the readme a bit

--- jquery-scrollfx.js
+++ jquery-scrollfx.js
@@ -1,5 +1,7 @@
 navbar_height = 50 // Your navbar height in PX (just don't type px right...)
-
+mavbar_lockClass = "toggled" // Lock navbar with this class
+navbar_hoverTrigger = ".navbar" // Select wich element on your page can trigger the navbar by hover, it needs to be a .class or #id
+navbar_target = ".navbar" // Your navbar .class or #id
 
 var saved_scroll = 0; // Keep user scroll to compare it later
 
@@ -9,22 +11,22 @@ $(document).ready(function() {
           if (new_scroll > saved_scroll){ // Compare if scrolled distance is higher (if it is then the navbar will be disabled)
               if ( new_scroll > 95){ // Be sure the user scrolled a bit before disabling navbar
                 toggleNavbar("off");
-                $(".navbar").removeClass("toggled");  
+                $(navbar_target).removeClass(navbar_lockClass);  
               } else { 
                   // pass
               }
             saved_scroll = new_scroll; // Gives saved scroll the new scroll value so we can use it later
           } else { // That's if new_scroll is lower than saved_scroll, meaning user has scrolled up so we enable the navbar
             toggleNavbar("on");
-            $(".navbar").addClass("toggled");
+            $(navbar_target).addClass(navbar_lockClass);
             saved_scroll = new_scroll; // Gives saved scroll the new scroll value so we can use it later
           }
     });
 
-    $(".navbar").hover(function(){ // If user hover the navbar (as a litle bit of the navbar is still on the viewport)
+    $(navbar_hoverTrigger).hover(function(){ // If user hover the navbar (as a litle bit of the navbar is still on the viewport)
       toggleNavbar("on"); // Activate navbar
     }, function(){ // When user stop hovering navbar
-        if ($(".navbar").hasClass("toggled")){ // if the navbar is toggled, means the user is at top of page or has changed page then do nothing
+        if ($(navbar_target).hasClass(navbar_lockClass)){ // if the navbar is toggled, means the user is at top of page or has changed page then do nothing
             // pass
         } else { // if the navbar is not toggled just disable it
             toggleNavbar("off");
@@ -34,8 +36,8 @@ $(document).ready(function() {
 
 function toggleNavbar(status){
     if ( status == "on"){
-      $(".navbar").css("transform", "translateY(0px)");
+      $(navbar_target).css("transform", "translateY(0px)");
     } else {
-      $(".navbar").css("transform", "translateY(-" + navbar_height + ")");
+      $(navbar_target).css("transform", "translateY(-" + navbar_height + ")");
     } 
 }
